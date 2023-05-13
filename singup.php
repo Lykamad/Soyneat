7a9de87ea9a5c3d0f423960c4028cdc71fee5c72
Lykamad <angelyka2001f@gmail.com>
2023-05-13 14:03:49 +0200

    cambios, adaptacion y fusion singup

--- singup.php
+++ singup.php
@@ -4,33 +4,33 @@ include("includes/a_config.php");
 
 <!DOCTYPE html>
 <html>
-
 <head>
-  <meta charset="UTF-8">
-  <link rel="stylesheet" type="text/css" href="estilo/unete.css">
+      <meta charset="UTF-8">
+      <link rel="stylesheet" type="text/css" href="estilo/unete.css">
 
 </head>
-
 <body>
   <?php
   include("includes/navigation.php");
   ?>
-  <h2>Inicia sesión</h2>
+  <h2>¡Regístrate!</h2>
   <div class="content">
   <form>
-    <label for="usuario">Usuario</label>
+    <label for="email">Correo electrónico</label>
+    <input type="text" id="pastillas" name="email"><br><br>
+    <label for="usuario">Nombre usuario</label>
     <input type="text" id="pastillas" name="usuario"><br><br>
     <label for="contrasena">Contraseña</label>
     <input type="password" id="pastillas" name="contrasena"><br><br>
     <input type="submit" id="enviar" value="Confirmar">
 
-    <p>¿No tienes una cuenta todavía? <a href="singup.php"><br><br>Regístrate</a></p>
-  </form><br>
+<p>¿Ya tienes una cuenta? <a href="login.php"><br><br>Inicia sesión</a></p>
+</form><br>
 
   <div class="panel">
-  <img src="imagenes/login_panel.png" width="500" height="400">
+  <img src="imagenes/singup_panel.png" width="500" height="400">
   </div>
-  </div>
+</div>
 </body>
 
 <?php
